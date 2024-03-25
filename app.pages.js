d8d4122f39e2bdbb2a4a66fcbf1111bd4b6a34c6
murray dong <murray@sharetreats.com>
2024-03-25 12:12:36 +0900

    init

---
+++ app.pages.js
@@ -0,0 +1,10 @@
+module.exports = {
+    index: {
+        template: 'public/templates/default.html',
+        title: 'Biz Treats'
+    },
+    'home' : {
+        template: 'public/templates/default.html',
+        title: 'Biz Treats'
+    },
+}
