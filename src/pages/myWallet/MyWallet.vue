33204330b9cafaec626c8ee1c09ea38202a9d337
eunjin0212 <eunjin9639@gmail.com>
2024-05-01 01:31:48 +0900

    🚧 myWallet 작업 진행중

--- src/pages/myWallet/MyWallet.vue
+++ src/pages/myWallet/MyWallet.vue
@@ -1,5 +1,6 @@
 <script>
 import { navMenu, snsMenu, menus, myPageLnbMenu } from '@/constants/components.js';
+import moment from 'moment';
 import SearchSvg from '@/assets/icons/SearchSvg.vue';
 import BellSvg from '@/assets/icons/BellSvg.vue';
 import CartSvg from '@/assets/icons/CartSvg.vue';
@@ -14,6 +15,9 @@ import MessageSvg from '@/assets/icons/MessageSvg.vue';
 import PhoneSvg from '@/assets/icons/PhoneSvg.vue';
 import BookmarkSvg from '@/assets/icons/BookmarkSvg.vue';
 import PinSvg from '@/assets/icons/PinSvg.vue';
+import UpSvg from '@/assets/icons/UpSvg.vue';
+import DownSvg from '@/assets/icons/DownSvg.vue';
+import Calendar from '@/assets/icons/Calendar.vue';
 
 export default {
     components: {
@@ -31,6 +35,9 @@ export default {
         PhoneSvg,
         BookmarkSvg,
         PinSvg,
+        UpSvg,
+        DownSvg,
+        Calendar,
     },
     data() {
         return {
@@ -64,6 +71,33 @@ export default {
                     read: false,
                 },
             ],
+            tableColumns: [
+                { label: 'Order ID', field: 'order_id', class: '' },
+                { label: 'Trans Info', field: 'trans_info', class: '' },
+                { label: 'Quantity', field: 'quantity', class: '' },
+                { label: 'Points', field: 'points', class: '' },
+                { label: 'Notes', field: 'notes', class: '' },
+                { label: 'Order Date', field: 'order_date', class: '' },
+                { label: 'Sent Date', field: 'sent_date', class: '' },
+                { label: 'Status', field: 'status', class: '' },
+                { label: 'Recipient List', field: 'recipient_list', class: '' },
+            ],
+            rows: [
+                {
+                    order_id: '202402260001',
+                    trans_info: 'McDonald’s Regular Coke McFloat',
+                    quantity: '200',
+                    points: '-1,280',
+                    notes: 'This is testing sending for check the template.',
+                    order_date: '02/31/2024 09:01',
+                    sent_date: '02/31/2024 09:01',
+                    status: 'Completed',
+                    recipient_list: '',
+                },
+            ],
+            moment,
+            fromDate: moment(new Date()).format('DD/MM/YYYY'),
+            toDate: moment(new Date()).format('DD/MM/YYYY')
         }
     },
     methods: {
@@ -208,10 +242,14 @@ export default {
                         <hr />
                         <ul class="wallet-info__card-content">
                             <li>
-                                <span><MessageSvg /></span>sena@sharetreats.com
+                                <span>
+                                    <MessageSvg />
+                                </span>sena@sharetreats.com
                             </li>
                             <li>
-                                <span><PhoneSvg /></span>09123456789
+                                <span>
+                                    <PhoneSvg />
+                                </span>09123456789
                             </li>
                         </ul>
                     </li>
@@ -223,10 +261,14 @@ export default {
                         <hr />
                         <ul class="wallet-info__card-content">
                             <li>
-                                <span><BookmarkSvg /></span>Share Treats Marketing
+                                <span>
+                                    <BookmarkSvg />
+                                </span>Share Treats Marketing
                             </li>
                             <li class="!leading-[14px]">
-                                <span><PinSvg /></span>Unit 501, BDO Equitable Tower, 8751Paseo De Roxas, Makati City, 1209
+                                <span>
+                                    <PinSvg />
+                                </span>Unit 501, BDO Equitable Tower, 8751Paseo De Roxas, Makati City, 1209
                             </li>
                         </ul>
                     </li>
@@ -237,8 +279,8 @@ export default {
                         </h3>
                         <hr class="border-gray100" />
                         <ul class="wallet-info__card-content ml-2.5 !pt-2.5">
-                            <li class="!text-[#A3A3A3] !text-sm leading-5 !font-bold !font-poppins !items-center">
-                                <i class="!text-[#4D4D4D] font-semibold font-inter leading-[48px] text-4xl">100,000</i>
+                            <li class="!text-point-unit !text-sm leading-5 !font-bold !font-poppins !items-center">
+                                <i class="!text-point font-semibold font-inter leading-[48px] text-4xl">100,000</i>
                                 Points
                             </li>
                         </ul>
@@ -247,9 +289,99 @@ export default {
             </div>
             <div class="section-card">
                 <h2><span></span>Summary of March</h2>
+                <ul class="flex items-center gap-[22px] px-3 mb-4.5">
+                    <li class="pt-5 pb-[9px] px-8 bg-[#B5E4CA40] rounded-xl flex-1">
+                        <p class="font-inter text-[#33383F] font-semibold text-[13px] leading-4">Top-up</p>
+                        <div class="flex items-end mb-3 mt-1">
+                            <i class="text-point font-inter font-bold text-5xl leading-[48px]">10,000</i>
+                            <span class="text-point-unit font-bold text-sm leading-5 font-poppins">Points</span>
+                        </div>
+                        <span
+                          class="bg-point-chip p-1 flex w-fit items-center gap-1 text-xs leading-4 font-bold text-point-up rounded -tracking-wide"
+                        >
+                            <UpSvg /> 37.8%
+                        </span>
+                    </li>
+                    <li class="pt-5 pb-[9px] px-8 bg-[#B1E5FC40] rounded-xl flex-1">
+                        <p class="font-inter text-[#33383F] font-semibold text-[13px] leading-4">Transfer</p>
+                        <div class="flex items-end mb-3 mt-1">
+                            <i class="text-point font-inter font-bold text-5xl leading-[48px]">-5,000</i>
+                            <span class="text-point-unit font-bold text-sm leading-5 font-poppins">Points</span>
+                        </div>
+                        <span
+                          class="bg-point-chip p-1 flex w-fit items-center gap-1 text-xs leading-4 font-bold text-point-down rounded -tracking-wide"
+                        >
+                            <DownSvg /> 37.8%
+                        </span>
+                    </li>
+                    <li class="pt-5 pb-[9px] px-8 bg-[#CABDFF40] rounded-xl flex-1">
+                        <p class="font-inter text-[#33383F] font-semibold text-[13px] leading-4">Used</p>
+                        <div class="flex items-end mb-3 mt-1">
+                            <i class="text-point font-inter font-bold text-5xl leading-[48px]">6,000</i>
+                            <span class="text-point-unit font-bold text-sm leading-5 font-poppins">Points</span>
+                        </div>
+                        <span
+                          class="bg-point-chip p-1 flex w-fit items-center gap-1 text-xs leading-4 font-bold text-point-up rounded -tracking-wide"
+                        >
+                            <UpSvg /> 37.8%
+                        </span>
+                    </li>
+                </ul>
             </div>
-            <div class="section-card">
-                <h2><span></span>Point History</h2>
+            <div class="section-card !px-0">
+                <h2 class="px-3"><span></span>Point History</h2>
+                <div class="table-function-bar">
+                    <label class="date-picker">
+                        <Calendar />
+                        {{ fromDate }}
+                        <input
+                          type="date"
+                          :value="fromDate"
+                          @change="event => fromDate = moment(event.target.value).format('DD/MM/YYYY')"
+                        />
+                    </label>
+                    <span class="date-range-symbol"></span>
+                    <label class="date-picker">
+                        <Calendar />
+                        {{ toDate }}
+                        <input
+                          type="date"
+                          :value="toDate"
+                          @change="event => toDate = moment(event.target.value).format('DD/MM/YYYY')"
+                        />
+                    </label>
+                    <div class="search-input">
+                        <input
+                          type="text"
+                          placeholder="Search for Treats"
+                          name="search"
+                          v-model="search"
+                        />
+                        <SearchSvg />
+                    </div>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th
+                              v-for="column in tableColumns"
+                              :key="column.field"
+                            >{{ column.label }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                          v-for="row in rows"
+                          :key="row.order_id"
+                        >
+                            <td
+                              v-for="column in tableColumns"
+                              :key="column.field"
+                            >{{ row[column.field] }}</td>
+                        </tr>
+                    </tbody>
+                    <tfoot></tfoot>
+                </table>
             </div>
         </section>
     </main>
