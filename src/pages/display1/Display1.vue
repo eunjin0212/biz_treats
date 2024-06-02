e62448a9dbd730c3643c4e229fdf88397e845db0
eunjin0212 <eunjin9639@gmail.com>
2024-06-03 00:20:42 +0900

    🚧 in progress display1

---
+++ src/pages/display1/Display1.vue
@@ -0,0 +1,379 @@
+<script>
+import { snsMenu, menus, mainMenu } from '@/constants/components.js';
+import { cartData } from '@/mock/cart';
+import { genBudgetMockData } from '@/mock/budget';
+import { alertData } from '@/mock/alertData.js'
+import SearchSvg from '@/assets/icons/SearchSvg.vue';
+import BellSvg from '@/assets/icons/BellSvg.vue';
+import CartSvg from '@/assets/icons/CartSvg.vue';
+import DropdownSvg from '@/assets/icons/DropdownSvg.vue';
+import WalletSvg from '@/assets/icons/WalletSvg.vue';
+import SwitchSvg from '@/assets/icons/SwitchSvg.vue';
+import SignOutSvg from '@/assets/icons/SignOutSvg.vue';
+import AlertSvg from '@/assets/icons/AlertSvg.vue';
+import PointSvg from '@/assets/icons/PointSvg.vue';
+import ReadSvg from '@/assets/icons/ReadSvg.vue';
+import ProductCartSvg from '@/assets/icons/ProductCartSvg.vue';
+import AllSvg from '@/assets/icons/AllSvg.vue'
+
+export default {
+    components: {
+        SearchSvg,
+        BellSvg,
+        CartSvg,
+        DropdownSvg,
+        WalletSvg,
+        SwitchSvg,
+        SignOutSvg,
+        AlertSvg,
+        PointSvg,
+        ReadSvg,
+        AllSvg,
+        ProductCartSvg,
+    },
+    data() {
+        return {
+            snsMenu,
+            mainMenu,
+            menus,
+            search: '',
+            dropdown: false,
+            alertOpen: false,
+            cartData,
+            alertData,
+            hotline: [
+                {
+                    title: 'Telephone number',
+                    content: '+63)917-712-4921',
+                },
+                {
+                    title: 'Email',
+                    content: 'biztreats@sharetreats.com',
+                },
+                {
+                    title: 'Business Hours',
+                    content: '10AM - 5PM (Monday-Friday)',
+                },
+            ],
+            noticeData: [
+                {
+                    title: 'Response',
+                    content: ['Our CS will contact the customers within 2 business days via email or phone.'],
+                },
+                {
+                    title: 'Resolution',
+                    content: ['Our CS will resolve the issue and contact the customers within 10 business days once the inquiry is received.', 'In any special case that more investigation is required, our CS will answer the customers for an estimated timeline.'],
+                },
+                {
+                    title: 'Refund',
+                    content: ['Refund will be made approximately within 10 business days depending on the payment issuer’s process once <br> the refund is confirmed by our CS.'],
+                },
+            ],
+            selectedFilter: {
+                budget: 0,
+                category: 'ALL'
+            },
+            categoryFilter: [
+                { label: 'ALL', icon: 'AllSvg', textClass: '' },
+                { label: 'eWallet', icon: '', textClass: '' },
+                { label: 'Grocery & Essentials', icon: '', textClass: '' },
+                { label: 'Fast Food', icon: '', textClass: 'text-nowrap' },
+                { label: 'Casual Resto', icon: '', textClass: '' },
+                { label: 'Bread & Dessert', icon: '', textClass: '' },
+                { label: 'Drugstore & Wellness', icon: '', textClass: '-tracking-[0.14px]' },
+                { label: 'Beauty & Lifestyle', icon: '', textClass: '' },
+                { label: 'Transpo & Travel', icon: '', textClass: '' },
+                { label: 'Digital & Appliance', icon: '', textClass: '' },
+                { label: 'Home & Kids', icon: '', textClass: '' },
+            ],
+            budgetFilter: [
+                { label: 'Below P50', min: 0, max: 50 },
+                { label: 'P51~P100', min: 51, max: 100 },
+                { label: 'P101~P300', min: 101, max: 300 },
+                { label: 'P301~P500', min: 301, max: 500 },
+                { label: 'P501 and UP', min: 501, max: 99999 },
+            ],
+            budgetRecommend: genBudgetMockData(1000),
+            budgetData: {},
+        }
+    },
+    methods: {
+        handleClick() {
+            window.location.href = '/login'
+        },
+        handleCartLocation() {
+            window.location.href = '/multiCart'
+        },
+        handleDropdown() {
+            this.dropdown = !this.dropdown
+        },
+        handleAlert() {
+            this.alertOpen = !this.alertOpen
+        },
+        handleGlobalDropdown(e) {
+            const { parentNode } = e.target
+            if (parentNode !== this.$refs.dropdownRef && !this.$refs.dropdownOptsRef.contains(parentNode)) {
+                this.dropdown = false
+            }
+        },
+        handleGlobalAlert(e) {
+            const { parentNode } = e.target
+            if (parentNode !== this.$refs.alertRef && !this.$refs.alertWrapperRef.contains(parentNode)) {
+                this.alertOpen = false
+            }
+        },
+    },
+    watch: {
+        dropdown() {
+            if (this.dropdown) {
+                window.addEventListener('click', this.handleGlobalDropdown)
+                return
+            }
+            window.removeEventListener('click', this.handleGlobalDropdown)
+        },
+        alertOpen() {
+            if (this.alertOpen) {
+                window.addEventListener('click', this.handleGlobalAlert)
+                return
+            }
+            window.removeEventListener('click', this.handleGlobalAlert)
+        },
+    },
+    computed: {
+        matchPath: () => {
+            return (path) => path === window.location.pathname
+        },
+    },
+    mounted() {
+        const budgetOriginArray = (min = 0, max = 50) => this.budgetRecommend.filter((item) => item.sale_price >= min && item.sale_price < max)
+        this.budgetData = this.budgetFilter.reduce((obj, data) => {
+            obj[data.min] = budgetOriginArray(data.min, data.max)
+            return obj
+        }, {})
+    },
+}
+</script>
+<template>
+    <header class="service-header">
+        <div class="!w-[1120px]">
+            <nav>
+                <a
+                  href="/"
+                  class="!mr-5"
+                >
+                    <img src="/assets/images/biz_treats_log.png" />
+                </a>
+                <div class="search-input !w-[442px]">
+                    <input
+                      type="text"
+                      placeholder="Search for Treats"
+                      name="search"
+                      v-model="search"
+                    />
+                    <SearchSvg />
+                </div>
+                <div class="relative">
+                    <button
+                      class="header-btn"
+                      ref="alertRef"
+                      @click="handleAlert"
+                    >
+                        <BellSvg />
+                        <span class="badge">{{ alertData.length }}</span>
+                    </button>
+                    <aside
+                      class="alert-wrapper"
+                      ref="alertWrapperRef"
+                      :class="alertOpen ? 'block' : 'hidden'"
+                    >
+                        <div class="alert-header">
+                            <h1>Notification</h1>
+                            <p>
+                                <span>
+                                    That’s all your notifications from the last 14days.
+                                </span>
+                                <button>
+                                    Mark all as read
+                                </button>
+                            </p>
+                        </div>
+                        <hr />
+                        <ul>
+                            <li
+                              v-for="(alert, idx) in alertData"
+                              :key="idx"
+                            >
+                                <div>
+                                    <span :class="alert.type === 'alert' ? 'bg-[#FCB1E7]' : 'bg-[#A7E5FF]'">
+                                        <AlertSvg v-if="alert.type === 'alert'" />
+                                        <PointSvg v-else />
+                                    </span>
+                                    <div>
+                                        <p>
+                                            <span>{{ alert.title }}</span>
+                                            <span>({{ alert.date }})</span>
+                                        </p>
+                                        <span>{{ alert.description }}</span>
+                                    </div>
+                                    <span
+                                      v-if="alert.read"
+                                      class="alert-check"
+                                    >
+                                        <ReadSvg />
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+                <button
+                  class="header-btn inline-flex ml-4.5"
+                  @click="() => handleCartLocation()"
+                >
+                    <CartSvg />
+                    <span class="text-[15px] leading-5 -tracking-[0.323px] font-bold font-inter ml-4 mr-1.5">{{
+                        cartData.length }}</span>
+                </button>
+                <div class="ml-4 btn-group">
+                    <button class="border-r border-r-[#197298]">Wallet Name</button>
+                    <div class="dropdown border-l border-l-[#60D0FF]">
+                        <button
+                          @click="handleDropdown"
+                          ref="dropdownRef"
+                        >
+                            <span>
+                                10000 points
+                            </span>
+                            <DropdownSvg />
+                        </button>
+                        <div
+                          class="dropdown-opts"
+                          ref="dropdownOptsRef"
+                          :class="dropdown ? 'block' : 'hidden'"
+                        >
+                            <a href="/myWallet">
+                                <WalletSvg />My Wallet
+                            </a>
+                            <a href="/chooseWallet">
+                                <SwitchSvg />Switching Wallet
+                            </a>
+                            <a href="/login">
+                                <SignOutSvg />Log out
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main class="flex flex-col w-full mx-auto">
+        <nav class="w-full main-menu min-w-[1440px]">
+            <div>
+                <a
+                  :class="{ 'active-main-menu': matchPath(menu.path) }"
+                  v-for="menu in mainMenu"
+                  :key="menu.title"
+                  :href="menu.path"
+                >{{ menu.title }}</a>
+            </div>
+        </nav>
+        <h1
+          class="text-[#6E6E6E] font-bold text-[32px] leading-[56px] -tracking-wide text-center py-[30px] border-b-2 border-b-[#CECECE] w-[1120px] mx-auto"
+        >Display1</h1>
+        <ul class="w-[1120px] mx-auto bg-white-20">
+            <li class="border-b border-b-[#EAEAEA] py-2">
+                <strong class="inline-block w-32 pl-6 text-[#696969] font-semibold text-sm">Price</strong>
+            </li>
+            <li class="border-b border-b-[#EAEAEA] py-2">
+                <strong class="inline-block w-32 pl-6 text-[#696969] font-semibold text-sm">Keyword</strong>
+            </li>
+            <li class="pr-3.5 py-2 text-right border-b border-b-gray-07">
+                <button>Search</button>
+            </li>
+        </ul>
+        <section class="pt-4 pb-40 main-section budget-section bg-white-18">
+            <div class="main-section__wrapper w-[1120px]">
+                <ul class="mb-12 product">
+                    <li
+                      v-for="(item, index) in budgetData[selectedFilter.budget]"
+                      :key="index"
+                    >
+                        <figure class="relative">
+                            <img
+                              :src="item.img"
+                              :alt="item.img"
+                            />
+                            <figcaption v-if="item.is_soldout">
+                                <span>SOLD OUT</span>
+                            </figcaption>
+                        </figure>
+                        <dl class="product__detail">
+                            <strong class="product__brand">{{ item.brand }}</strong>
+                            <dd class="product__name">{{ item.name }}</dd>
+                            <dd class="product__price">
+                                <strong class="product__price-sale">P{{ item.sale_price }}</strong>
+                                <s class="product__price-origin">P{{ item.price }}</s>
+                            </dd>
+                            <dd class="buttons">
+                                <button>Buy Now</button>
+                                <button>
+                                    <ProductCartSvg /> Add to Cart
+                                </button>
+                            </dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
+    <footer class="service-footer">
+        <div>
+            <aside>
+                <div>
+                    <img
+                      src="/assets/images/biz_treats_log.png"
+                      alt="footer-log"
+                    />
+                    <h4>
+                        {{ snsMenu.title }}
+                    </h4>
+                    <div>
+                        <a
+                          v-for="sns in snsMenu.children"
+                          :key="sns.name"
+                        >
+                            <img
+                              :src="sns.icon"
+                              :alt="sns.name"
+                            />
+                        </a>
+                    </div>
+                </div>
+                <div
+                  v-for="menu in menus"
+                  :key="menu.title"
+                >
+                    <h4>
+                        {{ menu.title }}
+                    </h4>
+                    <template
+                      v-for="sub in menu.children"
+                      :key="sub.title"
+                    >
+                        <a
+                          :href="sub.link"
+                          :target="sub.target"
+                          :alt="sub.name"
+                        >
+                            {{ sub.name }}
+                        </a>
+                    </template>
+                </div>
+            </aside>
+        </div>
+        <hr />
+        <p>
+            Copyright © SHARE TREATS. All rights reserved.
+        </p>
+    </footer>
+</template>
---
+++ src/pages/display1/display1.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import Display1 from "./Display1.vue";
+import '@/tailwind.css'
+
+createApp(Display1).mount("#app");
