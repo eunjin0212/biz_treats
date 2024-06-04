205758a026edb7891fdbd30ae8ddbf301f3954f2
eunjin0212 <eunjin9639@gmail.com>
2024-06-05 00:07:13 +0900

    🚧 in progress company website

--- src/pages/companyWebsite/CompanyWebsite.vue
+++ src/pages/companyWebsite/CompanyWebsite.vue
@@ -42,7 +42,7 @@ export default {
     methods: {
         handleClick() {
             window.location.href = '/login'
-        },        
+        },
         handleCartLocation() {
             window.location.href = '/multiCart'
         },
@@ -53,27 +53,27 @@ export default {
             this.alertOpen = !this.alertOpen
         },
         handleGlobalDropdown(e) {
-          const { parentNode } = e.target
+            const { parentNode } = e.target
             if (parentNode !== this.$refs.dropdownRef && !this.$refs.dropdownOptsRef.contains(parentNode)) {
                 this.dropdown = false
             }
         },
         handleGlobalAlert(e) {
-          const { parentNode } = e.target
+            const { parentNode } = e.target
             if (parentNode !== this.$refs.alertRef && !this.$refs.alertWrapperRef.contains(parentNode)) {
                 this.alertOpen = false
             }
         },
     },
-    watch:{
-        dropdown(){
+    watch: {
+        dropdown() {
             if (this.dropdown) {
                 window.addEventListener('click', this.handleGlobalDropdown)
                 return
             }
             window.removeEventListener('click', this.handleGlobalDropdown)
         },
-        alertOpen(){
+        alertOpen() {
             if (this.alertOpen) {
                 window.addEventListener('click', this.handleGlobalAlert)
                 return
@@ -99,7 +99,7 @@ export default {
                     <input
                       type="text"
                       placeholder="Search for Treats"
-@keypress.enter="() => handleSearch(search)"
+                      @keypress.enter="() => handleSearch(search)"
                       name="search"
                       v-model="search"
                     />
@@ -159,7 +159,10 @@ export default {
                         </ul>
                     </aside>
                 </div>
-                <button class="header-btn inline-flex ml-4.5" @click="() => handleCartLocation()">
+                <button
+                  class="header-btn inline-flex ml-4.5"
+                  @click="() => handleCartLocation()"
+                >
                     <CartSvg />
                     <span class="text-[15px] leading-5 -tracking-[0.323px] font-bold font-inter ml-4 mr-1.5">{{
                         cartData.length }}</span>
@@ -167,7 +170,10 @@ export default {
                 <div class="ml-4 btn-group">
                     <button class="border-r border-r-[#197298]">Wallet Name</button>
                     <div class="dropdown border-l border-l-[#60D0FF]">
-                        <button @click="handleDropdown" ref="dropdownRef">
+                        <button
+                          @click="handleDropdown"
+                          ref="dropdownRef"
+                        >
                             <span>
                                 10000 points
                             </span>
